4150f431573a2fcaea9eb2b32dcf5869196a9f33
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2022-11-11 08:08:14 +0530

    1st Commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("Hello Python")
+a=5
+b=6
+print(a+b) 
+a,b=b,a;#Swapping
+print(a)
+print(a==b)
+st='Tuhin\n is a good boy'
+print (st)
+a1=3+5j
+print(a1)
+print(type(a1))
+print(len(st))
+#substring
+print(st[1:10])
+print(st[-2])
+print(st.isupper())
