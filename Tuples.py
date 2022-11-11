a6f96ccf13852c25ec0bba5c7c0b443e268ae31c
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2022-11-11 08:09:02 +0530

    3rd Commit

---
+++ Tuples.py
@@ -0,0 +1,15 @@
+tup1=(1,"a",True,2,"b",False)
+#print(tup1)
+#print(tup1[1])
+#print(tup1[2:5])
+#print(tup1[-1])
+#length of tuple
+#print(len(tup1))
+tup2=(1,2,3,4)
+#print(tup1+tup2)
+print(tup1*3)
+print(tup1*3 +tup2)#repeat and concatenate
+#min and max values
+a=min(tup2)
+b=max(tup2)
+print(a,b)
