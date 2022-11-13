e0d2e9f7e9c3708819c65b49c7bc094768f8128a
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2022-11-13 11:32:15 +0530

    13th commit

---
+++ ifStatement.py
@@ -0,0 +1,36 @@
+"""
+a=10
+b=20
+if a>b :
+    print("a is greater than b")
+else :
+    print("b is greater than a")
+"""
+"""
+a=10 
+b=20 
+c=30
+if(a>b & a> c) :
+    print(a, " is the greatest")
+elif c>a :
+    print(c,"is greater")
+else :
+    print(a, "is not the greatest")
+"""
+"""
+tup=(1,2,3,"Hello")
+if 2 in tup:
+    print("Present")
+else:
+    print("Not present")
+"""
+"""
+li=[1,2,3,4,5]
+if li[1]==2:
+    li[1]=li[1]+li[2]
+print(li)
+"""
+dic={"a":1 , "b":2}
+if dic["b"]==2 :
+    dic["b"]=dic["b"]+5
+print (dic)
