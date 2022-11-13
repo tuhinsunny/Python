7396bdf0b12ce8df96665e28a4fa9577696cc5eb
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2022-11-13 11:33:41 +0530

    16th commit

---
+++ Loops.py
@@ -0,0 +1,34 @@
+"""
+i=1
+while i<10:
+    print(i,end=" ")
+    i+=1
+"""
+#table
+"""
+i=1
+while i<=10 :
+    print(2 , " * " , i , " = ", 2*i)
+    i+=1
+"""
+"""
+li=[1,2,3,4,5]
+i=0
+while i<len(li):
+    li[i]=li[i]+100
+    i+=1
+print(li)
+"""
+"""
+li=["apple","Peach","Banana"]
+list=[1,2,3,4,5]
+for i in li :
+    print(i)
+for i in li:
+    for j in list:
+        print(i,j)
+for i in range(10):
+    print(i+1)
+"""
+import platform
+print(platform.python_implementation{})
